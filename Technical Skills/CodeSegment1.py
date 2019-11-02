38f903e6393fececede038726c2aa1be353b47f2
Lmackenzie6 <54950424+Lmackenzie6@users.noreply.github.com>
2019-11-02 18:41:21 -0400

    Add files via upload

---
+++ Technical Skills/CodeSegment1.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import os
+
+root = tk.Tk()
+menubar = tk.Menu(root)
+
+
+print("Start program")
+root.configure(bg="#233C6A")
+
+root.title("ATL Tracker")
+
+
+root.mainloop()
+print ("End program")
+
+
+
+
+
+
+
+
