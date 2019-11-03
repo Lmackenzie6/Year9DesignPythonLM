c2e181a4afcf7724db169fd38543234c93cb997b
Lmackenzie6 <54950424+Lmackenzie6@users.noreply.github.com>
2019-11-03 13:04:54 -0500

    Add files via upload

---
+++ Technical Skills/CodeSegment3.py
@@ -0,0 +1,28 @@
+import tkinter as tk
+import os
+
+root = tk.Tk()
+
+
+print("Start program")
+root.configure(bg="#233C6A")
+
+button = tk.Button(text="Submit", fg="#EF5F36")
+
+button.pack(side=tk.TOP)
+
+
+
+root.title("ATL Tracker")
+
+
+root.mainloop()
+print ("End program")
+
+
+
+
+
+
+
+
